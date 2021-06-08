341c5f59d49e6f191a3faac1f00e525d33a407d0
zeynepyildiz1 <azeynepyildiz1@gmail.com>
2021-06-08 13:57:27 +0300

    check permutation

---
+++ PermutationCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PermutationCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
